912dd9b0eabedf86d4e2093789f7593f4c7fa741
zachi <zachi.greenberger@gmail.com>
2012-01-21 11:12:24 +0200

    non seo friendly mode, taking all folder images

--- Pages/ImageGallerySEO_friendly.aspx.cs
+++ Pages/ImageGallerySEO_friendly.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.IO;
 
 public partial class ImageGallery : System.Web.UI.Page
 {
@@ -36,7 +35,33 @@ public partial class ImageGallery : System.Web.UI.Page
 
     }
     
+//    public string LoadGalleryJS
+//    {
+//      get
+//      {
+//        int nNumberOfThumbnails = System.IO.Directory.GetFiles(MapPath(@"..\Images\ImgGallery\" + sCategory)).Length;
+//        return @" $(document).ready(function() {
+//                                        $('.ImageGallery').stoa({
+//                                                                    'numberOfThumbnails': " + nNumberOfThumbnails + @",
+//                                                                    'thumbnailsFolderName': '" + sCategory + @"'
+//                                                                   });
+//                                    });
+//                            ";
+//      }
+      
+//    }
 
+//    protected override void OnPreRender(EventArgs e)
+//    {
+//      base.OnPreRender(e);
+
+//      //ScriptManager.RegisterClientScriptInclude(this, typeof(Gallery), "jquery.ui.core.min.js", "/Controls/Gallery/jquery.ui.core.min.js");
+//      ScriptManager.RegisterClientScriptInclude(this, typeof(Gallery), "jquery.ui.widget.min.js", "/Controls/Gallery/jquery.ui.widget.min.js");
+//      ScriptManager.RegisterClientScriptInclude(this, typeof(Gallery), "galleryJs", "/Controls/Gallery/Gallery.plugin.js");
+//      ScriptManager.RegisterClientScriptInclude(this, typeof(Gallery), "gallery.mousewheel.js", "/Controls/Gallery/jquery.mousewheel.min.js");
+//      ScriptManager.RegisterClientScriptBlock(this, typeof(Gallery), "gallery.css", "<link rel='Stylesheet' type='text/css' href='/Controls/Gallery/Gallery.css' />", false);
+//      ScriptManager.RegisterClientScriptBlock(this, typeof(Gallery), "gallery.load.js", LoadGalleryJS, true);
+//    }
 
     public string LoadGalleryJS
     {
@@ -45,12 +70,7 @@ public partial class ImageGallery : System.Web.UI.Page
 
         return @"
                   $(document).ready(function () {
-                    var options = {
-                      thumbnailsFolderName: '" + ThumbnailsPath + @"', 
-                      numberOfThumbnails: " + NumberOfThumbnails + @",
-                      direction: 'rtl', 
-                    }
-                    $('div#turn-me-into-stoa-" + sCategory + @"').stoa(options);
+                    $('div#turn-me-into-stoa-" + sCategory + @"').stoa({ direction: 'rtl' });
                   });           
                ";
       }
@@ -68,21 +88,5 @@ public partial class ImageGallery : System.Web.UI.Page
       ScriptManager.RegisterClientScriptBlock(this, typeof(Gallery), "stoa.css", "<link rel='Stylesheet' type='text/css' href='/stoa/stoa.css' />", false);
       ScriptManager.RegisterClientScriptBlock(this, typeof(Gallery), "stoa.rtl.css", "<link rel='Stylesheet' type='text/css' href='/stoa/stoa.rtl.css' />", false);
       ScriptManager.RegisterClientScriptBlock(this, typeof(Gallery), "gallery.load.js", LoadGalleryJS, true);
-    }
-
-    public int NumberOfThumbnails
-    {
-      get
-      {
-        return Directory.GetFiles(MapPath(TemplateSourceDirectory + "/../" + ThumbnailsPath), "*.JPG", SearchOption.TopDirectoryOnly).Length;
-      }
-    }
-
-    public string ThumbnailsPath
-    {
-      get
-      {
-        return "/Images/ImgGallery/" + sCategory;
-      }
     }
 }
